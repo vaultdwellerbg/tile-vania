0739fba67d1008bbe538f78419b090aa8a7a7d30
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-10-24 20:13:07 +0300

    Add player horizontal movement

---
+++ Assets/Scripts/Player.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+public class Player : MonoBehaviour
+{
+	[SerializeField] float moveSpeed = 1f;
+
+	private Rigidbody2D rigidBody;
+	private Animator animator;
+
+	private void Start()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+		animator = GetComponent<Animator>();
+	}
+
+	private void Update()
+	{
+		Move();
+	}
+
+	private void Move()
+	{
+		float moveInputX = Input.GetAxis("Horizontal") * moveSpeed;
+		Vector2 playerVelocity = new Vector2(moveInputX, rigidBody.velocity.y);
+		rigidBody.velocity = playerVelocity;
+
+		animator.SetBool("isRunning", moveInputX != 0);
+	}
+}
