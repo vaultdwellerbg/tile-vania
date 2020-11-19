217fa9605ef9e983bd49f1fe4c59f8c96f753c20
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-11-19 17:05:57 +0200

    Add first enemy

---
+++ Assets/Scripts/EnemyMovement.cs
@@ -0,0 +1,38 @@
+﻿using UnityEngine;
+
+public class EnemyMovement : MonoBehaviour
+{
+	[SerializeField] float moveSpeed = 1f;
+
+	Rigidbody2D rigidBody;
+
+	private void Start()
+	{
+		rigidBody = GetComponent<Rigidbody2D>();
+	}
+
+	private void Update()
+	{
+		float direction = IsFacingRight() ? 1f : -1f;
+		rigidBody.velocity = new Vector2(moveSpeed * direction, rigidBody.velocity.y);
+	}
+
+	private void OnTriggerExit2D(Collider2D collision)
+	{
+		if (collision.gameObject.name == "Foreground")
+		{
+			FlipSprite();
+		}
+	}
+
+	private bool IsFacingRight()
+	{
+		return transform.localScale.x > 0f;
+	}
+
+	private void FlipSprite()
+	{
+		float newDirection = -Mathf.Sign(rigidBody.velocity.x);
+		transform.localScale = new Vector2(newDirection, 1f);
+	}
+}
