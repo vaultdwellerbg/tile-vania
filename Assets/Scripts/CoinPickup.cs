3ba1786cb1f5c0dd151f73faffcc9f2a57e4246a
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-12-22 01:32:02 +0200

    Add coin pickup

---
+++ Assets/Scripts/CoinPickup.cs
@@ -0,0 +1,19 @@
+﻿using UnityEngine;
+
+public class CoinPickup : MonoBehaviour
+{
+	private BoxCollider2D boxCollider;
+
+	private void Start()
+	{
+		boxCollider = GetComponent<BoxCollider2D>();
+	}
+
+	private void OnTriggerEnter2D(Collider2D collision)
+	{
+		if (boxCollider.IsTouchingLayers(LayerMask.GetMask("Player")))
+		{
+			Destroy(gameObject);
+		}
+	}
+}
