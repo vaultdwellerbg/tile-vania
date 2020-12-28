7c7054a6371ee0ff8b898423a88f32045aa98a10
Vaultdweller <vaultdwellerbg13@gmail.com>
2020-12-28 20:56:50 +0200

    Add levev flooding

---
+++ Assets/Scripts/RaisingLava.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine;
+
+public class RaisingLava : MonoBehaviour
+{
+	[Tooltip ("Game units per second")]
+    [SerializeField] float scrollRate = 0.3f;
+
+	private void Update()
+	{
+		transform.Translate(Vector2.up * Time.deltaTime * scrollRate);
+	}
+}
